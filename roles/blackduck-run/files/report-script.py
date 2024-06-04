fb4d08125ad8c4a3272ac650f7b518a1b0de78c1
Anton Lutteman <anton.lutteman@volvocars.com>
2024-06-04 11:03:22 +0200

    Adding dependency scan
    Change-Id: I768004272e3f700af6ab6e42e95a5418dc977989

---
+++ roles/blackduck-run/files/report-script.py
@@ -0,0 +1,44 @@
+import os
+import requests
+import re
+import time
+
+from ruamel.yaml import YAML
+from requests.adapters import HTTPAdapter, Retry
+
+yaml = YAML(typ='safe', pure=True)
+report_version = os.getenv("REPORT_VERSION", "")
+version = yaml.load(report_version)
+blackduck_token = os.getenv("BLACKDUCK_TOKEN", "")
+api_token = yaml.load(blackduck_token)
+blackduck_url = 'https://apus-blackduck.volvocars.biz'
+bearer_token_respons = requests.post(blackduck_url + "/api/tokens/authenticate", headers={'Authorization': 'token ' + api_token, 'Accept': 'application/vnd.blackducksoftware.user-4+json'})
+bearer_token = bearer_token_respons.json().get('bearerToken')
+data={
+  "reportFormat" : "TEXT",
+  "versionId" : version,
+  "categories" : [ "COPYRIGHT_TEXT" ],
+}
+create_notice_file_result = requests.post(blackduck_url + f"/api/versions/{version}/license-reports/", headers={'Authorization': 'Bearer ' + bearer_token, "Accept": "application/vnd.blackducksoftware.report-4+json" ,"Content-Type": "application/vnd.blackducksoftware.report-4+json"}, json=data)
+link_header = create_notice_file_result.headers.get('Link')
+urls = re.findall('<(.*?)>', link_header)
+rels = re.findall('rel="(.*?)"', link_header)
+link_dict = dict(zip(rels, urls))
+download_url = link_dict.get('download')
+time.sleep(60)
+session = requests.Session()
+retry = Retry(
+    total=10,
+    backoff_factor=10,
+    status_forcelist=[500, 502, 503, 504]
+)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('https://', adapter)
+response = session.get(download_url, headers={'Authorization': 'Bearer ' + bearer_token, "Accept": "application/vnd.blackducksoftware.report-4+json"})
+with open('status.txt', 'w') as f:
+    f.write(str(response.status_code))
+with open('headers.txt', 'w') as f:
+    for key, value in response.headers.items():
+        f.write(f"{key}: {value}\n")
+with open('license-report.zip', 'wb') as f:
+    f.write(response.content)
