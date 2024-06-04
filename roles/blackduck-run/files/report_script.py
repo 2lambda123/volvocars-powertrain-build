e998e40ddd59f00022287db364d6587f60369af6
Anton Lutteman <anton.lutteman@volvocars.com>
2024-06-04 11:03:22 +0200

    Adding dependency scan
    Change-Id: I768004272e3f700af6ab6e42e95a5418dc977989

---
+++ roles/blackduck-run/files/report_script.py
@@ -0,0 +1,88 @@
+"""Script generating and fetching notice report."""
+
+import os
+import re
+import time
+import requests
+from ruamel.yaml import YAML
+from requests.adapters import HTTPAdapter, Retry
+
+def get_bearer_token(blackduck_url, api_token):
+    """Get bearer token for Black Duck API."""
+
+    response = requests.post(
+        f"{blackduck_url}/api/tokens/authenticate",
+        headers={
+            'Authorization': f'token {api_token}',
+            'Accept': 'application/vnd.blackducksoftware.user-4+json'
+        }
+    )
+    return response.json().get('bearerToken')
+
+def create_notice_file(blackduck_url, bearer_token, version):
+    """Create notice file for the given version."""
+
+    data = {
+        "reportFormat": "TEXT",
+        "versionId": version,
+        "categories": ["COPYRIGHT_TEXT"],
+    }
+    response = requests.post(
+        f"{blackduck_url}/api/versions/{version}/license-reports/",
+        headers={
+            'Authorization': f'Bearer {bearer_token}',
+            "Accept": "application/vnd.blackducksoftware.report-4+json",
+            "Content-Type": "application/vnd.blackducksoftware.report-4+json"
+        },
+        json=data
+    )
+    return response.headers.get('Link')
+
+def extract_download_url(link_header):
+    """Extract download URL from the link header."""
+
+    urls = re.findall('<(.*?)>', link_header)
+    rels = re.findall('rel="(.*?)"', link_header)
+    link_dict = dict(zip(rels, urls))
+    return link_dict.get('download')
+
+def download_report(download_url, bearer_token, output_file):
+    """Download the notice report."""
+
+    session = requests.Session()
+    retry = Retry(
+        total=10,
+        backoff_factor=10,
+        status_forcelist=[500, 502, 503, 504]
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    response = session.get(
+        download_url,
+        headers={
+            'Authorization': f'Bearer {bearer_token}',
+            "Accept": "application/vnd.blackducksoftware.report-4+json"
+        }
+    )
+    with open(output_file, 'wb') as file:
+        file.write(response.content)
+
+def main():
+    """Main function."""
+
+    yaml = YAML(typ='safe', pure=True)
+    report_version = os.getenv("REPORT_VERSION", "")
+    version = yaml.load(report_version)
+    blackduck_token = os.getenv("BLACKDUCK_TOKEN", "")
+    api_token = yaml.load(blackduck_token)
+    blackduck_url = 'https://apus-blackduck.volvocars.biz'
+
+    bearer_token = get_bearer_token(blackduck_url, api_token)
+    link_header = create_notice_file(blackduck_url, bearer_token, version)
+    download_url = extract_download_url(link_header)
+
+    time.sleep(60) # Wait for the report to be generated
+    download_report(download_url, bearer_token, 'license-report.zip')
+
+if __name__ == "__main__":
+    main()
