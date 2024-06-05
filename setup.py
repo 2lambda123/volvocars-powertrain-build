7b374f5486de8a54c52c84fcf6befcf7c4d71de4
Henrik Wahlqvist <henrik.wahlqvist@volvocars.com>
2024-06-05 16:39:00 +0200

    Change later
    Change-Id: I7a861287c1d2515cd1a43c343437b3e6134956f9

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 # Copyright 2024 Volvo Car Corporation
 # Licensed under Apache 2.0.
 
-"""Create a python package for pybuild."""
+"""Create a python package for powertrain-build using setuptools and PBR."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    setup_requires=['pbr>=5.5.0'],
+    setup_requires=['pbr'],
     pbr=True,
-    python_requires='>=3.6, <3.11',
-    include_package_data=True,
-
 )
